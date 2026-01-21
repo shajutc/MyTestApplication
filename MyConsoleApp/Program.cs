1dbc3a83417304dcb314b62e06b27041c0752840
Shaju Thomas <shajutc@gmail.com>
2026-01-21 09:43:00 +0400

    First Commit

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string welcomemsg = "Hello World";
+string country = "UAE";
+
+if (country == "UIAE")
+    {welcomemsg = welcomemsg + ' ' + country;}
+else
+    {welcomemsg = welcomemsg + ' ' + "!!!!";}
+
+Console.WriteLine(welcomemsg);
+
+
